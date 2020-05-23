fc794f8c2bd4bab0ca1c188a7441031f5b310c2a
sa9ra4ma <hishihishi16006@gmail.com>
2020-05-23 18:04:30 +0900

    CORS対策

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/', (req:any, res:any) => res.send('Hello World!!'));
 
