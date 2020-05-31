eaf2b7dafd8632a81e5e72495dde2198950780fc
sa9ra4ma <hishihishi16006@gmail.com>
2020-05-31 14:11:35 +0900

    横浜のteam_idをdbからybに変更

--- src/client/components/TeamIconList.vue
+++ src/client/components/TeamIconList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="icon-bar">
     <ul class="nav-flex-container">
-      <li v-for="team in this.teamList" :key=team.id class="team-icon-li">
+      <li v-for="team in this.teamList" :key='team.id' class="team-icon-li">
         <router-link :to="{ name: 'Player', query: { id: team.id } }">
-          <img :src=team.icon_path class="team-icon">
+          <img :src='team.icon_path' class="team-icon">
         </router-link>
       </li>
     </ul>
@@ -16,7 +16,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class TeamIconList extends Vue {
-  private teamId: string[] = ['g', 'db', 't', 'c', 'd', 's', 'l', 'h', 'e', 'm', 'f', 'b']
+  private teamId: string[] = ['g', 'yb', 't', 'c', 'd', 's', 'l', 'h', 'e', 'm', 'f', 'b']
   private teamList: Object[] = [];
 
   private async created() {
