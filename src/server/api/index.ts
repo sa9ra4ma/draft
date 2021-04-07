1c51221382512013c8a4c7c29875fd600ef52902
sa9ra4ma <hishihishi16006@gmail.com>
2021-04-08 01:01:20 +0900

    メンバーAPIルーター設定もれ

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import {router as player} from './player'
-import {router as management} from './management';;
-import {router as room} from './room';;
+import {router as player} from './player';
+import {router as management} from './management';
+import {router as room} from './room';
+import {router as member} from './member';
 
 export const router = express.Router();
 
 router.use('/player', player);
 router.use('/m', management);
 router.use('/room', room);
+router.use('/member', member);
